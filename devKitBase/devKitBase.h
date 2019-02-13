d222146ff8d2ed62d0edd6b29f7a98fc8b8bf025
Megat Syafiq <megat@megat.local>
2019-02-13 14:44:57 +0800

    Initial Commit

---
+++ devKitBase/devKitBase.h
@@ -0,0 +1,19 @@
+//
+//  devKitBase.h
+//  devKitBase
+//
+//  Created by megat on 13/02/2019.
+//  Copyright © 2019 Megat Syafiq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for devKitBase.
+FOUNDATION_EXPORT double devKitBaseVersionNumber;
+
+//! Project version string for devKitBase.
+FOUNDATION_EXPORT const unsigned char devKitBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <devKitBase/PublicHeader.h>
+
+
